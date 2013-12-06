525c2f99309ec3539de012512acc35f246901743
YujinNam1 <namyoujin@gmail.com>
2013-12-07 06:34:28 +0900

    Calendar Not Fail ㅋㅋ

--- src/com/example/boogieboogie/EtcActivity.java
+++ src/com/example/boogieboogie/EtcActivity.java
@@ -1,13 +1,15 @@
+//ETC PAGE
 package com.example.boogieboogie;
 
-import android.content.Intent;
+
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
 
 public class EtcActivity extends Fragment {
 	public EtcActivity() {}
@@ -17,19 +19,38 @@ public class EtcActivity extends Fragment {
 			Bundle savedInstanceState) {
 		//TODO Auto-generated method stub
 		
+		
+		//SET VIEW
 		View view = inflater.inflate(R.layout.etc_activity, null);
 		
-		CalendarView calendar = (CalendarView)getView();
+		//dialog event
+		final Dialog dialog = new Dialog(getActivity());
+		dialog.setContentView(R.layout.dialog_calendar);
+		
+		CalendarView calendar = (CalendarView)view.findViewById(R.id.calendarview);
+		
+		//If click the date then Dialog 
+		calendar.setOnDateChangeListener(new OnDateChangeListener() {
+			
+			@Override
+			public void onSelectedDayChange(CalendarView view, int year, int month,
+					int dayOfMonth) {
+				// TODO Auto-generated method stub
+				dialog.show();
+				//Log.d("ok","year "+year+"   month -1 "+month+"    dayOfMonth"+dayOfMonth);
+				
+			}
+		});
+		
+		
 		//calendar.set
 				//findViewById(R.id.calendar_etc);
 		
-		Log.d("tag","111111111");
-		Intent intent = new Intent(getActivity(), CalenderActivity.class);
-		Log.d("tag","22222222");
-		this.startActivity(intent);
+		//Intent intent = new Intent(getActivity(), CalenderActivity.class);
+		//this.startActivity(intent);
 		
 		
 		return view;
 	}
-	
+
 }
