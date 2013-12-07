a03fda6b5986317ac920253f14f5084a5774301f
YujinNam1 <namyoujin@gmail.com>
2013-12-07 16:22:28 +0900

    etc완성

--- src/com/example/boogieboogie/BoogieBoogie.java
+++ src/com/example/boogieboogie/BoogieBoogie.java
@@ -11,10 +11,6 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class BoogieBoogie extends FragmentActivity implements
 		ActionBar.TabListener {
--- src/com/example/boogieboogie/EtcActivity.java
+++ src/com/example/boogieboogie/EtcActivity.java
@@ -1,56 +1,76 @@
 //ETC PAGE
 package com.example.boogieboogie;
 
-
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EtcActivity extends Fragment {
-	public EtcActivity() {}
-	
+	public EtcActivity() {
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		//TODO Auto-generated method stub
-		
-		
-		//SET VIEW
+		// TODO Auto-generated method stub
+
+		// SET VIEW
 		View view = inflater.inflate(R.layout.etc_activity, null);
-		
-		//dialog event
-		final Dialog dialog = new Dialog(getActivity());
-		dialog.setContentView(R.layout.dialog_calendar);
-		
-		CalendarView calendar = (CalendarView)view.findViewById(R.id.calendarview);
-		
-		//If click the date then Dialog 
+		// dialog event
+		//final Dialog dialog = new Dialog(getActivity());
+		//dialog.setContentView(R.layout.dialog_calendar);
+
+		CalendarView calendar = (CalendarView) view
+				.findViewById(R.id.calendarview);
+
+		// If click the date then Dialog
 		calendar.setOnDateChangeListener(new OnDateChangeListener() {
-			
+
 			@Override
-			public void onSelectedDayChange(CalendarView view, int year, int month,
-					int dayOfMonth) {
+			public void onSelectedDayChange(CalendarView view, int year,
+					int month, int dayOfMonth) {
 				// TODO Auto-generated method stub
-				dialog.show();
-				//Log.d("ok","year "+year+"   month -1 "+month+"    dayOfMonth"+dayOfMonth);
+
+				// If print date, need add 1 to Month
+				String str = (month + 1) + " " + dayOfMonth + ", " + year;
+				Log.d("tag", "1111111");
+				//Toast.makeText(getActivity().getBaseContext(), str,
+				//		Toast.LENGTH_SHORT).show();
+				//TextView tv = (TextView) dialog.findViewById(R.id.text_dialog);
+				// tv.setText();
+				//GridView gv = (GridView)dialog.findViewById(R.id.gridview_calendar);
 				
+				
+				
+				Intent intent = new Intent(getActivity(), ImageGridActivityOnCalendar.class);
+				Bundle data = new Bundle();
+				data.putString("data", str);
+				intent.putExtras(data);
+				getActivity().startActivity(intent);
+						
+				//dialog.setTitle("Book of " + str);
+				
+				//dialog.show();
+
 			}
 		});
-		
-		
-		//calendar.set
-				//findViewById(R.id.calendar_etc);
-		
-		//Intent intent = new Intent(getActivity(), CalenderActivity.class);
-		//this.startActivity(intent);
-		
-		
+
+		// calendar.set
+		// findViewById(R.id.calendar_etc);
+
+		// Intent intent = new Intent(getActivity(), CalenderActivity.class);
+		// this.startActivity(intent);
+
 		return view;
 	}
-
 }
