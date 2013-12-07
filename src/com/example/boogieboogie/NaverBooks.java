06411b24c7bb12e3bfa93d41875fc67d8f52e0a1
Joshua Jung <wbqdinno@gmail.com>
2013-12-08 03:36:32 +0900

    Intergrating Process 1

--- src/com/example/boogieboogie/NaverBooks.java
+++ src/com/example/boogieboogie/NaverBooks.java
@@ -1,6 +1,7 @@
 package com.example.boogieboogie;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,11 +14,14 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -140,7 +144,8 @@ public class NaverBooks extends Activity {
 									ioException.printStackTrace();
 								}
 								// 파일로 로컬하게 저장하는 것도 여기서 해야 함
-								saveAsFile(image, title, bm);
+								//saveAsFile(image, title, bm);
+								saveToInternalStorage(title, bm);
 							}
 						})
 				.setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -153,9 +158,46 @@ public class NaverBooks extends Activity {
 		alert.setTitle("Confirm");
 		alert.show();
 	}
+	/*
+	public void saveImage(String imageURL, String fileName, Bitmap image) {
+		//File file = 
+		FileOutputStream fos = openFileOutput(fileName+".jpg", Context.MODE_PRIVATE);
+		fos.write(image.get);
+		
+	}*/
+	private String saveToInternalStorage (String name, Bitmap bitmapImage) {
+		ContextWrapper cw = new ContextWrapper(getApplicationContext());
+		File directory = cw.getDir("Test", Context.MODE_PRIVATE);
+		File mypath = new File (directory, name+".jpg" );
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(mypath);
+			Log.i("save", "1");
+			bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Log.i("save", directory.getAbsolutePath());
+		return directory.getAbsolutePath();
+	}
 	
+	/*
+	public boolean saveImageInternalStorage(Context context, String imageURL, String fileName, Bitmap fileImg) throws IOException{
+		BufferedOutputStream out = new BufferedOutputStream(context.openFileOutput(fileName, 0));
+		fileImg.compress(CompressFormat.JPEG, 100, out);
+		out.flush();
+		out.close();
+		
+		return true;
+	}*/
+	/*
 	public void saveAsFile(String imageURL, String fileName, Bitmap fileImg) {
+<<<<<<< HEAD
 		String filePath = "/Download/" + fileName + ".jpg";
+=======
+		String filePath = "/test/" + fileName + ".jpg";
+>>>>>>> origin/HJ2
 		// File file = new File(Environment.getExternalStorageDirectory(),
 		// fileName+".jpg");
 		File file = new File(filePath);
@@ -169,8 +211,12 @@ public class NaverBooks extends Activity {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+<<<<<<< HEAD
 		Toast.makeText(this, filePath, Toast.LENGTH_SHORT).show();
 	}
+=======
+	}*/
+>>>>>>> origin/HJ2
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
