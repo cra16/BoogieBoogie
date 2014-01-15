f94121436dbfd48f4bfcc9e456fecc391c9b916a
wbqd <wbqdinno@gmail.com>
2014-01-15 14:21:09 +0900

    삭제

--- src/com/example/boogieboogie/FindActivity.java
+++ src/com/example/boogieboogie/FindActivity.java
@@ -1,15 +1,5 @@
 package com.example.boogieboogie;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
-
 public class FindActivity extends Fragment implements OnClickListener {
 	public FindActivity() {}
 	
